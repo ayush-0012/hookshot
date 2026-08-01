adb8646825eb3813a841d1d534880afdbfccfba5
ayush-0012 <ayushbisht0012@gmail.com>
2026-08-01 20:28:52 +0530

    adding IP in the job payload from ingestion api for rate limitting for the worker

--- apps/server/src/controllers/ingestion.controller.ts
+++ apps/server/src/controllers/ingestion.controller.ts
@@ -1,6 +1,5 @@
 import { db } from "@/db";
 import { payload } from "@/db/schema";
-import { testingWorker } from "@/services/worker.service";
 import { tryCatch } from "@/utils/handlers/tryCatch";
 import { redisClient } from "@/utils/redis";
 import { Queue } from "bullmq";
@@ -12,12 +11,18 @@ const payloadQueue = new Queue("payload-queue", {
 });
 
 export async function ingestion(req: Request, res: Response) {
-  const { payloadBody, endpoint, eventType, apiKey, signingkey } = req.body;
+  const { payloadBody, eventType, endpointId } = req.body;
 
-  if (!apiKey && !signingkey) {
-    return res
-      .status(401)
-      .json({ message: "Unauthorized, missing credentials" });
+  if (!endpointId) {
+    return res.status(401).json({ message: "EndpointId is missing" });
+  }
+
+  if (!eventType) {
+    return res.status(401).json({ message: "Event type is missing" });
+  }
+
+  if (!payloadBody) {
+    return res.status(401).json({ message: "Payload data is missing" });
   }
 
   console.log("validating json ");
@@ -33,26 +38,32 @@ export async function ingestion(req: Request, res: Response) {
 
   // insert the payload in the db first
   const { data: payloadRes, error: insertErr } = await tryCatch(
-    db.insert(payload).values({
-      payloadBody,
-      eventType,
-      payloadStatus: null, // it'll be updated later, after the service has returned a res
-      userId,
-    }),
+    db
+      .insert(payload)
+      .values({
+        payloadBody,
+        eventType,
+        payloadStatus: null, // it'll be updated later, after the service has returned a res
+        userId,
+      })
+      .returning(),
   );
+
+  const payloadId = payloadRes[0].id;
 
   console.log("queuing starts");
   if (payloadRes !== null) {
     const job = await payloadQueue.add(
       "payload",
-      { body: payloadBody, endpoint },
-      { jobId: uuid() },
+      { body: payloadBody, payloadId, eventType, endpointId, ip: req.ip },
+      {
+        jobId: uuid(),
+        attempts: 3,
+        backoff: { type: "exponential", delay: 10000 },
+      },
     );
 
     console.log("job id added to the queue", job?.id);
-
-    const workerRes = testingWorker();
-    console.log("worker res", workerRes);
 
     return res.status(200).json({ message: "added in queue" });
   } else {
