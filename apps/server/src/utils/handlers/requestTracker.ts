41d58c597eb694b4d73a1154361e8eaed00aefcc
ayush-0012 <ayushbisht0012@gmail.com>
2026-08-06 14:25:54 +0530

    updated name for relevance

---
+++ apps/server/src/utils/handlers/requestTracker.ts
@@ -0,0 +1,36 @@
+import { db } from "@/db";
+import { logs } from "@/db/schema";
+import { tryCatch } from "./tryCatch";
+
+// it will track the jobs status and store the data for that in the db
+export async function requestTracker(
+  attemptsMade: number,
+  endpointId: string,
+  payloadId: string,
+  statusCode: number,
+  endpointRes: unknown,
+) {
+  console.log("db call to save in logs");
+  const endpointResponse =
+    typeof endpointRes === "string"
+      ? endpointRes
+      : JSON.stringify(endpointRes ?? {});
+
+  const { data: logsInsert, error: insertErr } = await tryCatch(
+    db
+      .insert(logs)
+      .values({
+        endpointId,
+        payloadId,
+        statusCode,
+        attemptNumber: attemptsMade + 1,
+        endpointResponse,
+      })
+      .returning(),
+  );
+
+  if (logsInsert) console.log("logs insert in db", logsInsert);
+  else console.log("logs insert err", insertErr);
+
+  return logsInsert;
+}
