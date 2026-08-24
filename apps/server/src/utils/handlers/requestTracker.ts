94b0c75e272efd5a8e1c1f4623ab11a61e10959b
ayush-0012 <ayushbisht0012@gmail.com>
2026-08-25 00:39:53 +0530

    updated the request tracker to store the failure categorya and reason for ECCON

--- apps/server/src/utils/handlers/requestTracker.ts
+++ apps/server/src/utils/handlers/requestTracker.ts
@@ -5,26 +5,34 @@ import { tryCatch } from "./tryCatch";
 // it will track the jobs status and store the data for that in the db
 export async function requestTracker(
   attemptsMade: number,
+  userId: string,
   endpointId: string,
   payloadId: string,
   statusCode: number,
   endpointRes: unknown,
+  failureCategory?: string,
+  failureReason?: string,
 ) {
   console.log("db call to save in logs");
   const endpointResponse =
     typeof endpointRes === "string"
       ? endpointRes
       : JSON.stringify(endpointRes ?? {});
+  const startedAt = new Date().toISOString();
 
   const { data: logsInsert, error: insertErr } = await tryCatch(
     db
       .insert(logs)
       .values({
+        userId,
         endpointId,
         payloadId,
         statusCode,
         attemptNumber: attemptsMade + 1,
         endpointResponse,
+        failureCategory,
+        failureReason,
+        startedAt,
       })
       .returning(),
   );
